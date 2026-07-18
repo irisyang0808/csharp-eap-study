70899c9d91ba0ee4bb0abb825a4a8100d244112a
irisyang0808 <iris88.yang@gmail.com>
2026-07-18 21:39:21 +0800

    day1: class and object basics - Equipment example

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Equipment eq1 = new Equipment();
+eq1.ID = "EQP01";
+eq1.Start();
+
+Console.WriteLine(eq1.ID);
+Console.WriteLine(eq1.Status);
+eq1.Stop();
+Console.WriteLine(eq1.Status);
+
+public class Equipment
+{
+    public string ID { get; set; }
+    public string Status { get; set; }
+
+    public void Start()
+    {
+        Status = "RUN";
+    }
+    public void Stop()
+    {
+        Status = "IDLE";
+    }
+}
